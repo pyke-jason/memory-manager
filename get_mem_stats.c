5eca4b1322b65adc3470ee8d5db024b47cef40fc
Ara Vardanyan <araaa@uw.edu>
2022-05-17 22:04:09 -0700

    skeleton

---
+++ get_mem_stats.c
@@ -0,0 +1,14 @@
+/*
+ * Ara Vardanyan, Jason Pyke
+ * CSE 374 22sp HW 6
+ * Memory Manager
+ * get_mem_stats.c
+ * implementation of get_mem_stats function
+*/
+
+#include <mem.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void get_mem_stats(uintptr_t* total_size, uintptr_t* total_free, uintptr_t* n_free_blocks) {
+}
