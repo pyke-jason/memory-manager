cd3e64e4e7ff63421b2de78399d73cd30fe62916
Ara Vardanyan <araaa@uw.edu>
2022-05-17 22:03:35 -0700

    skeleton

---
+++ bench.c
@@ -0,0 +1,19 @@
+/*
+ * Ara Vardanyan, Jason Pyke
+ * CSE 374 22sp HW 6
+ * Memory Manager
+ * bench.c
+ * benchmarking/test program for Memory Manager
+ * executes a large number of calls to functions getmem and freemem to allocate and
+ * free blocks of random sizes and in random order. Allow the user to specify parameters that control the test.
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <mem.h>
+#include <mem_impl.h>
+
+int main(int argc, char **argv) {
+    return EXIT_SUCCESS;
+}
