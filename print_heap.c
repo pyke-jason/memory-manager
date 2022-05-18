59acf1306edf9787e4ba99f7b6ba12d7fe57b825
Ara Vardanyan <araaa@uw.edu>
2022-05-17 22:06:05 -0700

    skeleton

---
+++ print_heap.c
@@ -0,0 +1,14 @@
+/*
+ * Ara Vardanyan, Jason Pyke
+ * CSE 374 22sp HW 6
+ * Memory Manager
+ * print_heap.c
+ * implementation of print_heap function
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <mem_impl.h>
+
+void print_heap(FILE * f) {
+}
