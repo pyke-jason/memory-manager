7d6549ec03247cbd84385786caba6285f0532006
Ara Vardanyan <araaa@uw.edu>
2022-05-17 22:05:51 -0700

    skeleton

---
+++ mem_utils.c
@@ -0,0 +1,15 @@
+/*
+ * Ara Vardanyan, Jason Pyke
+ * CSE 374 22sp HW 6
+ * Memory Manager
+ * mem_utils.c
+ * implementation of check_heap function
+*/
+
+#include <mem.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <mem_impl.h>
+
+void check_heap() {
+}
