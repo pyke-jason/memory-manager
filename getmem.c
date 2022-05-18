9e738b462fa1c2415309d18a2ae61ea2759ef0a9
Ara Vardanyan <araaa@uw.edu>
2022-05-17 22:04:32 -0700

    skeleton

---
+++ getmem.c
@@ -0,0 +1,15 @@
+/*
+ * Ara Vardanyan, Jason Pyke
+ * CSE 374 22sp HW 6
+ * Memory Manager
+ * getmem.c
+ * implementation of getmem function
+*/
+
+#include <mem.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void* getmem(uintptr_t size) {
+    return NULL;
+}
