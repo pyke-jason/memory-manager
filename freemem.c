ec0d97f22359f9381f2e130a8bf2d15d19f6999b
Ara Vardanyan <araaa@uw.edu>
2022-05-17 22:03:52 -0700

    skeleton

---
+++ freemem.c
@@ -0,0 +1,14 @@
+/*
+ * Ara Vardanyan, Jason Pyke
+ * CSE 374 22sp HW 6
+ * Memory Manager
+ * freemem.c
+ * implementation of freemem function
+*/
+
+#include <mem.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void freemem(void* p) {
+}
