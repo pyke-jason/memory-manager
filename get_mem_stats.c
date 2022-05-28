dc87e639960fc0b97a05f7102edfe0ecbc47159b
Ara Vardanyan <araaa@uw.edu>
2022-05-27 19:03:51 -0700

    Replace get_mem_stats.c

--- get_mem_stats.c
+++ get_mem_stats.c
@@ -7,9 +7,12 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
-
-#include "mem.h"
+#include <mem.h>
+#include <mem_impl.h>
 
 void get_mem_stats(uintptr_t* total_size, uintptr_t* total_free,
-                   uintptr_t* n_free_blocks) {}
+                   uintptr_t* n_free_blocks) {
+    *total_size = total_size_glob;
+    *total_free = total_free_glob;
+    *n_free_blocks = n_free_blocks_glob;
+}
